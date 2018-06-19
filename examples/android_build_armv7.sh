09ff623ef1540967e863a0cc2a871dce0f824208
haitao <wanght.cn@gmail.com>
2018-06-19 16:40:43 +0800

    fixup android build issues
    Former-commit-id: 27a3b8b4f655e527f7df329ede256f0769936c0a

--- examples/android_build_armv7.sh
+++ examples/android_build_armv7.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-export ANDROID_NDK=/home/zhangrui/android-ndk-r16
+export ANDROID_NDK=/home/usr/android-ndk-r16
 
 cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake \
      -DANDROID_ABI="armeabi-v7a" \
      -DANDROID_PLATFORM=android-21 \
      -DANDROID_STL=gnustl_shared \
-     -DTENGINE_DIR=/home/zhangrui/zr_tengine/tengine \
-     -DOpenCV_DIR=/home/zhangrui/OpenCV-android-sdk/sdk/native/jni \
-     -DPROTOBUF_DIR=/home/zhangrui/oaid_tengine/tengine/protobuf_lib \
+     -DTENGINE_DIR=/home/usr/tengine \
+     -DOpenCV_DIR=/home/usr/OpenCV-android-sdk/sdk/native/jni \
+     -DPROTOBUF_DIR=/home/usr/protobuf_lib \
+     -DBLAS_DIR=/home/usr/openbla020_android \
      ..
 
