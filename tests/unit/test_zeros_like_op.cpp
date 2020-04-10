bddd81a17044f69a3ee906d6cc61e01e004ecf14
shunqi <2775613813@qq.com>
2020-04-10 16:36:25 +0800

    CI add 20 files

---
+++ tests/unit/test_zeros_like_op.cpp
@@ -0,0 +1,215 @@
+#include <unistd.h>
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <vector>
+
+#include "tengine_c_api.h"
+#include "compiler_fp16.h"
+
+int create_input_node(graph_t graph)
+{
+    node_t node = create_graph_node(graph, "input1", "InputOp");
+    tensor_t tensor = create_graph_tensor(graph, "input1", TENGINE_DT_FP32);
+
+    if(tensor == nullptr)
+    {
+        release_graph_node(node);
+        return -1;
+    }
+
+    set_node_output_tensor(node, 0, tensor, TENSOR_TYPE_INPUT);
+
+    int dims[4] = {1, 4, 4, 5};
+
+    set_tensor_shape(tensor, dims, 4);
+
+    release_graph_tensor(tensor);
+    release_graph_node(node);
+
+    return 0;
+}
+
+int create_test_node(graph_t graph)
+{
+    node_t test_node = create_graph_node(graph, "ZerosLike", "ZerosLike");
+
+    tensor_t input_tensor_1= get_graph_tensor(graph, "input1");
+
+    if((input_tensor_1 == nullptr))
+    {
+        std::cout << "ERRNO: " << get_tengine_errno() << "\n";
+        return -1;
+    }
+
+    set_node_input_tensor(test_node, 0, input_tensor_1);
+
+    release_graph_tensor(input_tensor_1);
+
+    /* output */
+    tensor_t output_tensor = create_graph_tensor(graph, "ZerosLike", TENGINE_DT_FP32);
+
+    set_node_output_tensor(test_node, 0, output_tensor, TENSOR_TYPE_VAR);
+
+    release_graph_tensor(output_tensor);
+
+    release_graph_node(test_node);
+
+    return 0;
+}
+
+graph_t create_test_graph()
+{
+    graph_t graph = create_graph(nullptr, nullptr, nullptr);
+
+    if(graph == nullptr)
+    {
+        std::cerr << "create failed: ERRNO: " << get_tengine_errno() << "\n";
+        return nullptr;
+    }
+
+    if(set_graph_layout(graph, TENGINE_LAYOUT_NHWC) < 0)
+    {
+        std::cerr << "set layout failed: ERRNO: " << get_tengine_errno() << "\n";
+        return nullptr;
+    }
+
+    if(create_input_node(graph) < 0)
+    {
+        std::cerr << "create input failed\n";
+        return nullptr;
+    }
+
+    if(create_test_node(graph) < 0)
+    {
+        std::cerr << "create test node failed\n";
+        return nullptr;
+    }
+
+    /* set input/output node */
+    const char* inputs[] = {"input1"};
+    const char* outputs[] = {"ZerosLike"};
+
+    if(set_graph_input_node(graph, inputs, 1) < 0)
+    {
+        std::cerr << "set inputs failed: ERRNO: " << get_tengine_errno() << "\n";
+        return nullptr;
+    }
+
+    if(set_graph_output_node(graph, outputs, 1) < 0)
+    {
+        std::cerr << "set outputs failed: ERRNO: " << get_tengine_errno() << "\n";
+        return nullptr;
+    }
+
+    return graph;
+}
+
+void* set_input_data(graph_t graph)
+{
+    tensor_t input_tensor = get_graph_input_tensor(graph, 0, 0);
+
+    int buf_size = get_tensor_buffer_size(input_tensor);
+
+    // input_tensor_1
+
+    void* i_buf = malloc(buf_size);
+
+    int dims1[4];
+
+    get_tensor_shape(input_tensor, dims1, 4);
+
+    int elem_num = dims1[0] * dims1[1] * dims1[2] * dims1[3];
+    int data_type = get_tensor_data_type(input_tensor);
+
+    for(int i = 0; i < elem_num; i++)
+    {
+        if(data_type == TENGINE_DT_FP32)
+        {
+            float* f = ( float* )i_buf;
+            f[i] = i;
+        }
+    }
+
+    set_tensor_buffer(input_tensor, i_buf, buf_size);
+
+    release_graph_tensor(input_tensor);
+
+    return i_buf;
+}
+
+void dump_output_data(node_t test_node)
+{
+    tensor_t output_tensor = get_node_output_tensor(test_node, 0);
+    int dims[4];
+
+    get_tensor_shape(output_tensor, dims, 4);
+
+    void* o_buf = get_tensor_buffer(output_tensor);
+    int data_type = get_tensor_data_type(output_tensor);
+    
+    int flag = 0;
+    for(int i = 0; i < dims[0] * dims[1] * dims[2] * dims[3]; i++)
+    {
+        if(data_type == TENGINE_DT_FP32)
+        {
+            float* p = ( float* )o_buf;
+            // std::cout << p[i] << std::endl;
+            if(p[i] != 0)
+            {
+	            flag = 1;
+	        }
+        }
+        
+    }
+    if(flag == 0){
+        std::cout << "pass\n";
+    }
+    else{
+    	std::cout << "test failed\n";
+    }
+    release_graph_tensor(output_tensor);
+}
+
+int main(int argc, char* argv[])
+{  
+    init_tengine();
+
+    graph_t graph = create_test_graph();
+
+    if(graph == nullptr)
+        return 1;
+
+    /* set input */
+    void* i_buf = set_input_data(graph);
+
+    // prerun graph
+    if(prerun_graph(graph) < 0)
+    {
+        std::cerr << "prerun_graph failed: ERRNO: " << get_tengine_errno() << "\n";
+        return 1;
+    }
+    dump_graph(graph);
+    node_t test_node = get_graph_node(graph, "ZerosLike");
+
+    const char* dev = get_node_device(test_node);
+
+    std::cout << "node running on dev: " << dev << "\n";
+
+    if(run_graph(graph, 1) < 0)
+    {
+        std::cerr << "run_graph failed: ERRNO: " << get_tengine_errno() << "\n";
+        return 2;
+    }
+    // dump_graph(graph);
+    free(i_buf);
+    dump_output_data(test_node);
+
+    release_graph_node(test_node);
+
+    postrun_graph(graph);
+    destroy_graph(graph);
+
+    release_tengine();
+    return 0;
+}
