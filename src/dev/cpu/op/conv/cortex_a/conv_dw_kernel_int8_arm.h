e4e554f2a663c1861b8e13794bd8a963bad45b7c
shitouren1994 <49087929+shitouren1994@users.noreply.github.com>
2020-11-07 10:47:40 +0800

    添加int8的convdw实现:3x3s1和3x3s2 (#470)
    * 添加int8convdw:3x3s1和3x3s2
    
    Co-authored-by: shitouren1994 <shihebing@bigo.sg>

---
+++ src/dev/cpu/op/conv/cortex_a/conv_dw_kernel_int8_arm.h
@@ -0,0 +1,14 @@
+//
+// Created by Hebing Shi on 2020/11/4.
+//
+
+#ifndef TENGINLITE_CONV_DW_KERNEL_INT8_ARM_H
+#define TENGINLITE_CONV_DW_KERNEL_INT8_ARM_H
+
+#include "tengine_ir.h"
+#include "convolution_param.h"
+
+int conv_dw_int8_run(struct ir_tensor* input_tensor, struct ir_tensor* filter_tensor, struct ir_tensor* bias_tensor,
+                     struct ir_tensor* output_tensor, struct conv_param* param, int num_thread, int cpu_affinity);
+
+#endif    // TENGINLITE_CONV_DW_KERNEL_INT8_ARM_H
