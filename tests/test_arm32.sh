ebb5a9e11d2c7fb663f1a2599a97403ad39f1fff
BUG1989 <248857878@qq.com>
2021-01-03 09:26:12 +0800

    try to fix the bugs of CI on arm32/64

--- tests/test_arm32.sh
+++ tests/test_arm32.sh
@@ -4,7 +4,7 @@ export NumClusterLite=1
 
 function classify_cat()
 {
-    qemu-arm -L /usr/arm-linux-gnueabi ./build/tests/tm_classify -n $1
+    qemu-arm -L /usr/arm-linux-gnueabi ../tests/tm_classify -n $1
     if [ "$?" != 0 ]; then
         echo "failed"
         return 0
@@ -16,7 +16,7 @@ function classify_cat()
 
 function classify_bike()
 {
-    qemu-arm -L /usr/arm-linux-gnueabi ./build/tests/tm_classify -n $1 -i ./images/bike.jpg
+    qemu-arm -L /usr/arm-linux-gnueabi ../tests/tm_classify -n $1 -i ./images/bike.jpg
     if [ "$?" != 0 ]; then
         echo "failed"
         return 0
