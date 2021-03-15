3319dc420ba2cbf0d9998deab4802511f6ef5719
Di Wu <stevenwudi@gmail.com>
2021-03-15 19:37:14 +0800

    Mish uint8 fake quantization added. (#582)

--- src/dev/cpu/op/mish/mish_ref.c
+++ src/dev/cpu/op/mish/mish_ref.c
@@ -30,6 +30,61 @@
 #include "../../cpu_node_ops.h"
 #include "tengine_op.h"
 
+int ref_mish_uint8(struct ir_tensor *input_tensor, struct ir_tensor *output_tensor, int num_thread)
+{
+    int w = input_tensor->dims[3];
+    int h = output_tensor->dims[2];
+    int channels = input_tensor->dims[1];
+    int batch = input_tensor->dims[0];
+
+    int size = h * w;
+    int c_step = h * w;
+    int batch_step = c_step * channels;
+    int total_size = batch_step * batch;
+
+    // dequant
+    uint8_t* input_uint8 = input_tensor->data;
+    uint8_t* output_uint8 = output_tensor->data;
+    float input_scale = input_tensor->scale;
+    float output_scale = output_tensor->scale;
+    int32_t input_zero = input_tensor->zero_point;
+    int32_t output_zero = output_tensor->zero_point;
+
+    float* data_fp32 = sys_malloc(total_size * sizeof(float));
+
+    for(int i = 0; i < total_size; i++)
+        data_fp32[i] = ((float) input_uint8[i] - (float)input_zero) * input_scale;
+
+
+    for (int n = 0; n < batch; n++)
+    {
+//#pragma omp parallel for num_threads(num_thread)
+        for (int q = 0; q < channels; q++)
+        {
+            float* src = data_fp32 + batch_step * n + c_step * q;
+            float* dst = data_fp32 + batch_step * n + c_step * q;
+
+            for (int i = 0; i < size; i++)
+            {
+                dst[i] = src[i] * tanhf(log(1 + exp(src[i])));
+            }
+        }
+    }
+
+    // quant
+    for(int i=0; i<total_size; i++)
+    {
+        int udata = round(data_fp32[i] / output_scale + output_zero);
+        if (udata > 255)
+            udata = 255;
+        else if (udata < 0)
+            udata = 0;
+        output_uint8[i] = udata;
+    }
+    sys_free(data_fp32);
+    return 0;
+}
+
 int ref_mish_fp32(struct ir_tensor* input_tensor, struct ir_tensor* output_tensor, int num_thread)
 {
     int w = input_tensor->dims[3];
@@ -66,6 +121,7 @@ static int release_node(struct node_ops* node_ops, struct exec_node* exec_node, 
     return 0;
 }
 
+
 static int run(struct node_ops* node_ops, struct exec_node* exec_node, struct exec_graph* exec_graph)
 {
     struct ir_node* ir_node = exec_node->ir_node;
@@ -76,9 +132,13 @@ static int run(struct node_ops* node_ops, struct exec_node* exec_node, struct ex
     input_tensor = get_ir_graph_tensor(ir_graph, ir_node->input_tensors[0]);
     output_tensor = get_ir_graph_tensor(ir_graph, ir_node->output_tensors[0]);
 
-    ref_mish_fp32(input_tensor, output_tensor, exec_graph->num_thread);
+    int ret = -1;
+    if(input_tensor->data_type == TENGINE_DT_FP32)
+        ret = ref_mish_fp32(input_tensor, output_tensor, exec_graph->num_thread);
+    else if(input_tensor->data_type == TENGINE_DT_UINT8)
+        ret = ref_mish_uint8(input_tensor, output_tensor, exec_graph->num_thread);
 
-    return 0;
+    return ret;
 }
 
 static int reshape(struct node_ops* node_ops, struct exec_node* exec_node, struct exec_graph* exec_graph)
