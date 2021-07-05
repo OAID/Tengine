d0a9ddc1ba826a98989e8bae26fb68ce356c7660
xlchen <xlchen@openailab.com>
2021-07-05 14:54:49 +0800

    support tensor consumer num more than 8 for tensor reuse.

--- source/graph/node.c
+++ source/graph/node.c
@@ -182,7 +182,7 @@ int set_ir_node_input_tensor(ir_node_t* node, int input_idx, ir_tensor_t* tensor
 {
     if (TE_MAX_CONSUMER_NUM <= tensor->consumer_num)
     {
-        return -1;
+        set_ir_tensor_consumer_num(tensor, tensor->consumer_num + 1);
     }
 
     if (input_idx >= node->input_num)
