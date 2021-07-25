71d95101d595b11f8df5118d73fa997744dfe060
daquexian <daquexian566@gmail.com>
2021-07-25 10:56:09 +0800

    build convert tool appimage in ci
    Signed-off-by: daquexian <daquexian566@gmail.com>

---
+++ .github/workflows/build-package-and-test-convert-tools.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -ex
+
+yum install -y centos-release-scl
+yum install -y devtoolset-7 python3 python3-pip ImageMagick file fuse-libs
+pip3 install -U --user pip
+python3 -m pip install cmake
+
+source /opt/rh/devtoolset-7/enable
+
+PROTOBUF_REPO_DIR=/home/zhangjh/repos/protobuf
+PROTOBUF_BUILD_DIR=build-for-ci
+TENGINE_BUILD_DIR=build-for-ci
+
+pushd $PROTOBUF_REPO_DIR
+mkdir -p $PROTOBUF_BUILD_DIR
+pushd $PROTOBUF_BUILD_DIR
+cmake ../cmake
+cmake --build . -j`nproc`
+cmake -DCMAKE_INSTALL_PREFIX=install -P cmake_install.cmake
+popd
+popd
+
+export CMAKE_PREFIX_PATH=$PROTOBUF_REPO_DIR/$PROTOBUF_BUILD_DIR/install
+mkdir -p $TENGINE_BUILD_DIR
+pushd $TENGINE_BUILD_DIR
+cmake -DTENGINE_BUILD_CONVERT_TOOL=ON ..
+cmake --build . -j`nproc`
+popd
+
+SCRIPT_DIR=`dirname "$0"`
+
+source $SCRIPT_DIR/generate-appimage.sh $TENGINE_BUILD_DIR/tools/convert_tool/convert_tool
