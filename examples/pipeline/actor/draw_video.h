04058282b6d1a314ccd0d65b950b2207e8725c25
BUG1989 <BUG1989@users.noreply.github.com>
2021-07-27 06:37:32 +0000

    apply code-format changes

--- examples/pipeline/actor/draw_video.h
+++ examples/pipeline/actor/draw_video.h
@@ -27,23 +27,29 @@
 
 namespace pipe {
 
-class DrawVideo : public Node<Param<cv::Mat>, Param<void>> {
+class DrawVideo : public Node<Param<cv::Mat>, Param<void> >
+{
 public:
-  void exec() override {
-    cv::Mat mat;
-    while (true) {
-
-      auto suc = input<0>()->pop(mat);
-      if (not suc) {
-        continue;
-      }
-      // fprintf(stdout, "show\n");
-      cv::imshow("camera", mat);
-      cv::waitKey(25);
+    void exec() override
+    {
+        cv::Mat mat;
+        while (true)
+        {
+            auto suc = input<0>()->pop(mat);
+            if (not suc)
+            {
+                continue;
+            }
+            // fprintf(stdout, "show\n");
+            cv::imshow("camera", mat);
+            cv::waitKey(25);
+        }
     }
-  }
 
-  ~DrawVideo() { cv::destroyAllWindows(); }
+    ~DrawVideo()
+    {
+        cv::destroyAllWindows();
+    }
 };
 
 } // namespace pipe
--- examples/pipeline/actor/video_camera.h
+++ examples/pipeline/actor/video_camera.h
@@ -30,60 +30,72 @@
 
 namespace pipe {
 
-class VideoCamera : public Node<Param<void>, Param<cv::Mat>> {
+class VideoCamera : public Node<Param<void>, Param<cv::Mat> >
+{
 public:
-  VideoCamera(const std::string video_path = "") : m_path(video_path) {
-  }
+    VideoCamera(const std::string video_path = "")
+        : m_path(video_path)
+    {
+    }
 
-  void exec() override {
-    std::call_once(flag, [&]() {
-      cv::VideoCapture cap;
-      if (m_path.empty()) {
-        cap.open(0);
-      } else {
-        cap.open(m_path);
-      }
+    void exec() override
+    {
+        std::call_once(flag, [&]() {
+            cv::VideoCapture cap;
+            if (m_path.empty())
+            {
+                cap.open(0);
+            }
+            else
+            {
+                cap.open(m_path);
+            }
 
-      if (not cap.isOpened()) {
-        fprintf(stderr, "cannot open video %s\n", m_path.c_str());
-        return;
-      }
+            if (not cap.isOpened())
+            {
+                fprintf(stderr, "cannot open video %s\n", m_path.c_str());
+                return;
+            }
 
-      double rate = cap.get(CV_CAP_PROP_FPS);
-      fprintf(stdout, "rate %lf\n", rate);
-      fprintf(stdout, "pan %lf\n", cap.get(CV_CAP_PROP_PAN));
-      fprintf(stdout, "width = %.2f\n",cap.get(CV_CAP_PROP_FRAME_WIDTH));
-      fprintf(stdout, "height = %.2f\n",cap.get(CV_CAP_PROP_FRAME_HEIGHT));
-      fprintf(stdout, "fbs = %.2f\n",cap.get(CV_CAP_PROP_FPS));
-      fprintf(stdout, "brightness = %.2f\n",cap.get(CV_CAP_PROP_BRIGHTNESS));
-      fprintf(stdout, "contrast = %.2f\n",cap.get(CV_CAP_PROP_CONTRAST));
-      fprintf(stdout, "saturation = %.2f\n",cap.get(CV_CAP_PROP_SATURATION));
-      fprintf(stdout, "hue = %.2f\n",cap.get(CV_CAP_PROP_HUE));
-      fprintf(stdout, "exposure = %.2f\n",cap.get(CV_CAP_PROP_EXPOSURE));
+            double rate = cap.get(CV_CAP_PROP_FPS);
+            fprintf(stdout, "rate %lf\n", rate);
+            fprintf(stdout, "pan %lf\n", cap.get(CV_CAP_PROP_PAN));
+            fprintf(stdout, "width = %.2f\n", cap.get(CV_CAP_PROP_FRAME_WIDTH));
+            fprintf(stdout, "height = %.2f\n", cap.get(CV_CAP_PROP_FRAME_HEIGHT));
+            fprintf(stdout, "fbs = %.2f\n", cap.get(CV_CAP_PROP_FPS));
+            fprintf(stdout, "brightness = %.2f\n", cap.get(CV_CAP_PROP_BRIGHTNESS));
+            fprintf(stdout, "contrast = %.2f\n", cap.get(CV_CAP_PROP_CONTRAST));
+            fprintf(stdout, "saturation = %.2f\n", cap.get(CV_CAP_PROP_SATURATION));
+            fprintf(stdout, "hue = %.2f\n", cap.get(CV_CAP_PROP_HUE));
+            fprintf(stdout, "exposure = %.2f\n", cap.get(CV_CAP_PROP_EXPOSURE));
 
-      while (true) {
-        cv::Mat mat;
-        if (not cap.read(mat)) {
-          break;
-        }
+            while (true)
+            {
+                cv::Mat mat;
+                if (not cap.read(mat))
+                {
+                    break;
+                }
 
-        if (mat.empty()) {
-          break;
-        }
+                if (mat.empty())
+                {
+                    break;
+                }
 
-        auto success = output<0>()->try_push(mat.clone());
-        if (not success) {
-          fprintf(stdout, "drop " __FILE__ "\n");
-        }
-      }
+                auto success = output<0>()->try_push(mat.clone());
+                if (not success)
+                {
+                    fprintf(stdout, "drop " __FILE__ "\n");
+                }
+            }
 
-      cv::waitKey(1000 / std::max(1.0, rate));
-    });
-  }
+            cv::waitKey(1000 / std::max(1.0, rate));
+        });
+    }
 
 private:
-  std::string m_path;
-  std::once_flag flag;
+    std::string m_path;
+    std::once_flag flag;
 };
 
 } // namespace pipe
