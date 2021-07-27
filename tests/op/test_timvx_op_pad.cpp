d579474ba2b3df5a32d27044e021308b3cbf5b7d
clovking <clovking@users.noreply.github.com>
2021-07-27 02:54:01 +0000

    apply code-format changes

--- tests/op/test_timvx_op_pad.cpp
+++ tests/op/test_timvx_op_pad.cpp
@@ -22,7 +22,6 @@
  * Author: qwang02@openailab.com
  */
 
-
 #include "test_op.h"
 #include "graph/graph.h"
 #include "graph/node.h"
@@ -45,14 +44,18 @@ static void get_uint8_data(float* data_fp32, uint8_t* date_u8, int size, float s
 
 int create_test_pad_node(graph_t graph, const char* input_name, const char* node_name, int data_type, int layout, int n, int c, int h, int w)
 {
-    (void)layout; (void)n; (void)c; (void)h; (void)w;
+    (void)layout;
+    (void)n;
+    (void)c;
+    (void)h;
+    (void)w;
 
     /* create the test node */
     node_t test_node = create_graph_node(graph, node_name, "Pad");
 
     tensor_t input_tensor = get_graph_tensor(graph, input_name);
 
-    if(NULL == input_tensor)
+    if (NULL == input_tensor)
     {
         fprintf(stderr, "create test node failed.\n");
         return -1;
@@ -66,7 +69,7 @@ int create_test_pad_node(graph_t graph, const char* input_name, const char* node
     set_node_output_tensor(test_node, 0, output_tensor, TENSOR_TYPE_VAR);
 
     /* set params */
-    struct pad_param* pad_param = ( struct pad_param* )((struct node* )test_node)->op.param_mem;
+    struct pad_param* pad_param = (struct pad_param*)((struct node*)test_node)->op.param_mem;
     pad_param->mode = 0;
     pad_param->pad_0_h = 0;
     pad_param->pad_0_w = 0;
@@ -88,20 +91,18 @@ int create_test_pad_node(graph_t graph, const char* input_name, const char* node
  * float32 = (uint8 - zero_point) * scale
  */
 float input_fp32[9] = {
-                            3.0f, 8.0f, 1.0f, 
-                            9.0f, 5.0f, 7.0f, 
-                            3.0f, 2.0f, 3.0f
-                       };
+    3.0f, 8.0f, 1.0f,
+    9.0f, 5.0f, 7.0f,
+    3.0f, 2.0f, 3.0f};
 float input_scale = 1.0f;
 int input_zero_point = 0;
 
 float reference_out[25] = {
-                            0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
-                            0.0f, 3.0f, 8.0f, 1.0f, 0.0f,
-                            0.0f, 9.0f, 5.0f, 7.0f, 0.0f,
-                            0.0f, 3.0f, 2.0f, 3.0f, 0.0f,
-                            0.0f, 0.0f, 0.0f, 0.0f, 0.0f
-                         };
+    0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+    0.0f, 3.0f, 8.0f, 1.0f, 0.0f,
+    0.0f, 9.0f, 5.0f, 7.0f, 0.0f,
+    0.0f, 3.0f, 2.0f, 3.0f, 0.0f,
+    0.0f, 0.0f, 0.0f, 0.0f, 0.0f};
 float output_scale = 1.0f;
 int output_zero_point = 0;
 
@@ -119,7 +120,7 @@ int main(int argc, char* argv[])
 
     // create
     graph_t graph = create_timvx_test_graph(test_node_name, data_type, layout, n, c, h, w, &create_test_pad_node);
-    if(NULL == graph)
+    if (NULL == graph)
         return -1;
 
     // set quantize params
@@ -142,17 +143,17 @@ int main(int argc, char* argv[])
 
     // get output and dequant
     struct tensor* output_tensor = (struct tensor*)get_graph_output_tensor(graph, 0, 0);
-    uint8_t* output_u8 = ( uint8_t*) output_tensor->data;
+    uint8_t* output_u8 = (uint8_t*)output_tensor->data;
     int output_size = 25;
 
     get_tensor_quant_param(output_tensor, &output_scale, &output_zero_point, 1);
-    float* output_data = ( float* )malloc(output_size * sizeof(float));
+    float* output_data = (float*)malloc(output_size * sizeof(float));
     for (int i = 0; i < output_size; i++)
-        output_data[i] = (( float )output_u8[i] - ( float )output_zero_point) * output_scale;
+        output_data[i] = ((float)output_u8[i] - (float)output_zero_point) * output_scale;
 
     // check the result
     ret = 0;
-    for (int i = 0; i< output_size; i++)
+    for (int i = 0; i < output_size; i++)
     {
         if (fabsf(output_data[i] - reference_out[i]) > 0.01)
         {
--- tests/op/test_timvx_op_reduction.cpp
+++ tests/op/test_timvx_op_reduction.cpp
@@ -22,7 +22,6 @@
  * Author: qwang02@openailab.com
  */
 
-
 #include "test_op.h"
 #include "graph/graph.h"
 #include "graph/node.h"
@@ -45,14 +44,18 @@ static void get_uint8_data(float* data_fp32, uint8_t* date_u8, int size, float s
 
 int create_test_reduction_node(graph_t graph, const char* input_name, const char* node_name, int data_type, int layout, int n, int c, int h, int w)
 {
-    (void)layout; (void)n; (void)c; (void)h; (void)w;
+    (void)layout;
+    (void)n;
+    (void)c;
+    (void)h;
+    (void)w;
 
     /* create the test node */
     node_t test_node = create_graph_node(graph, node_name, "Reduction");
 
     tensor_t input_tensor = get_graph_tensor(graph, input_name);
 
-    if(NULL == input_tensor)
+    if (NULL == input_tensor)
     {
         fprintf(stderr, "create test node failed.\n");
         return -1;
@@ -66,7 +69,7 @@ int create_test_reduction_node(graph_t graph, const char* input_name, const char
     set_node_output_tensor(test_node, 0, output_tensor, TENSOR_TYPE_VAR);
 
     /* set params */
-    struct reduction_param* reduction_param = ( struct reduction_param* )((struct node* )test_node)->op.param_mem;
+    struct reduction_param* reduction_param = (struct reduction_param*)((struct node*)test_node)->op.param_mem;
     reduction_param->type = 0;
     reduction_param->dim_0 = 1;
     reduction_param->dim_1 = -2;
@@ -82,11 +85,11 @@ int create_test_reduction_node(graph_t graph, const char* input_name, const char
  * uint8   = clip(round(float32 / scale) + zero_point, 0, 255)
  * float32 = (uint8 - zero_point) * scale
  */
-int dims[5] = {1,2,2,3,2};
+int dims[5] = {1, 2, 2, 3, 2};
 
-float input_fp32[24] = { 3.0f, 8.0f, 1.0f, 9.0f, 5.0f, 0.0f, 3.0f, 5.0f,
+float input_fp32[24] = {3.0f, 8.0f, 1.0f, 9.0f, 5.0f, 0.0f, 3.0f, 5.0f,
                         5.0f, 0.0f, 1.0f, 5.0f, 5.0f, 4.0f, 3.0f, 5.0f,
-                        2.0f, 3.0f, 4.0f, 2.0f, 4.0f, 3.0f, 3.0f, 5.0f };
+                        2.0f, 3.0f, 4.0f, 2.0f, 4.0f, 3.0f, 3.0f, 5.0f};
 float input_scale = 1.0f;
 int input_zero_point = 0;
 
@@ -108,7 +111,7 @@ int main(int argc, char* argv[])
 
     // create
     graph_t graph = create_timvx_test_graph(test_node_name, data_type, layout, n, c, h, w, &create_test_reduction_node);
-    if(NULL == graph)
+    if (NULL == graph)
         return -1;
 
     // set quantize params
@@ -129,7 +132,6 @@ int main(int argc, char* argv[])
     get_uint8_data(input_fp32, input_u8, 24, input_scale, input_zero_point);
     set_tensor_buffer(input_tensor, input_u8, 24);
 
-
     // graph run
     ret = test_graph_run(graph);
     if (0 != ret)
@@ -140,17 +142,17 @@ int main(int argc, char* argv[])
     }
 
     // get output and dequant
-    uint8_t* output_u8 = ( uint8_t*) output_tensor->data;
+    uint8_t* output_u8 = (uint8_t*)output_tensor->data;
     int output_size = 12;
 
     get_tensor_quant_param(output_tensor, &output_scale, &output_zero_point, 1);
-    float* output_data = ( float* )malloc(output_size * sizeof(float));
+    float* output_data = (float*)malloc(output_size * sizeof(float));
     for (int i = 0; i < output_size; i++)
-        output_data[i] = (( float )output_u8[i] - ( float )output_zero_point) * output_scale;
+        output_data[i] = ((float)output_u8[i] - (float)output_zero_point) * output_scale;
 
     // check the result
     ret = 0;
-    for (int i = 0; i< output_size; i++)
+    for (int i = 0; i < output_size; i++)
     {
         if (fabsf(output_data[i] - reference_out[i]) > 0.01)
         {
