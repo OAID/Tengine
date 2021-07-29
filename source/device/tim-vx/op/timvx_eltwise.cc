684f52b763bcfc82f45ba5a3c9fd471642fe02b1
Your Name <you@example.com>
2021-07-29 17:12:59 +0800

    Update eltwise const timvx

--- source/device/tim-vx/op/timvx_eltwise.cc
+++ source/device/tim-vx/op/timvx_eltwise.cc
@@ -28,6 +28,8 @@ extern "C"
 {
 #include "operator/op.h"
 #include "eltwise_param.h"
+
+#include "../common/compiler_fp16.h"
 }
 
 
@@ -35,9 +37,13 @@ bool VXEngine::AddEltwiseNode(struct node* ir_node)
 {
     struct graph* ir_graph = ir_node->graph;
 
+    struct tensor* input_tensor0 = get_ir_graph_tensor(ir_graph, ir_node->input_tensors[0]);
+
     struct tensor* const_tensor = nullptr;
     if (ir_node->input_num > 1)
         const_tensor = get_ir_graph_tensor(ir_graph, ir_node->input_tensors[1]);
+
+    eltwise_param* param = (eltwise_param*)ir_node->op.param_mem;
 
     if (nullptr != const_tensor && const_tensor->tensor_type == TENSOR_TYPE_CONST && const_tensor->data_type == TENGINE_DT_FP32)
     {
@@ -45,7 +51,6 @@ bool VXEngine::AddEltwiseNode(struct node* ir_node)
         int const_size = get_tensor_buffer_size(const_tensor) / sizeof(float) ;
         if (const_size == 1 && const_fp32[0] == 0)
         {
-            struct tensor* input_tensor = get_ir_graph_tensor(ir_graph, ir_node->input_tensors[0]);
             struct tensor* output_tensor = get_ir_graph_tensor(ir_graph, ir_node->output_tensors[0]);
             std::vector<uint32_t> perm;
             for (int i = output_tensor->dim_num - 1; i >= 0; i--)
@@ -57,68 +62,120 @@ bool VXEngine::AddEltwiseNode(struct node* ir_node)
             vx_node_map[ir_node->index] = reshape;
 
             (*reshape)
-                    .BindInputs({ this->vx_tensor_map[input_tensor->index] })
+                    .BindInputs({ this->vx_tensor_map[input_tensor0->index] })
                     .BindOutputs({ this->vx_tensor_map[output_tensor->index] });
+        }
+        else if (const_size == 1 && const_fp32[0] != 0)
+        {
+            struct tensor* input_tensor1 = get_ir_graph_tensor(ir_graph, ir_node->input_tensors[1]);
+
+            float data_fp32 = ((float*)get_tensor_buffer(input_tensor1))[0];
+            __fp16 data_fp16 = fp32_to_fp16(data_fp32);
+
+            __fp16* fp16_data = (__fp16*)malloc(input_tensor0->elem_num * sizeof(__fp16) );
+            for (int k = 0; k < input_tensor0->elem_num; k++)
+            {
+                fp16_data[k] = data_fp16;
+            }
+
+            tim::vx::ShapeType vx_shape;
+            for (int i = input_tensor0->dim_num - 1; i >= 0; i--)
+            {
+                vx_shape.push_back(input_tensor0->dims[i]);
+            }
+
+            tim::vx::TensorSpec vx_spec(tim::vx::DataType::FLOAT16, vx_shape,
+                                        tim::vx::TensorAttribute::CONSTANT);
+            auto vx_tensor = this->graph->CreateTensor(vx_spec, fp16_data);
+
+            this->vx_tensor_map[input_tensor1->index] = vx_tensor;
+        }
+        else if (const_size == input_tensor0->dims[1])
+        {
+            struct tensor* input_tensor1 = get_ir_graph_tensor(ir_graph, ir_node->input_tensors[1]);
+
+            float* data_fp32 = (float*)get_tensor_buffer(input_tensor1);
+
+            __fp16* fp16_data = (__fp16*)malloc(input_tensor0->elem_num * sizeof(__fp16) );
+            for (int p = 0; p < input_tensor0->dims[1]; p++)
+            {
+                for (int k = 0; k < input_tensor0->elem_num / input_tensor0->dims[1]; k++)
+                {
+                    __fp16 data_fp16 = fp32_to_fp16(data_fp32[p]);
+                    fp16_data[k] = data_fp16;
+                }
+            }
+
+            tim::vx::ShapeType vx_shape;
+            for (int i = input_tensor0->dim_num - 1; i >= 0; i--)
+            {
+                vx_shape.push_back(input_tensor0->dims[i]);
+            }
+
+            tim::vx::TensorSpec vx_spec(tim::vx::DataType::FLOAT16, vx_shape,
+                                        tim::vx::TensorAttribute::CONSTANT);
+            auto vx_tensor = this->graph->CreateTensor(vx_spec, fp16_data);
+
+            this->vx_tensor_map[input_tensor1->index] = vx_tensor;
         }
         else
         {
             //to do
+            fprintf(stderr,"To do!!\n");
         }
 
     }
-    else
+
+    std::vector<std::shared_ptr<tim::vx::Tensor> > add_in_tensor(ir_node->input_num);
+    for (int i = 0; i < ir_node->input_num; i++)
     {
-        std::vector<std::shared_ptr<tim::vx::Tensor> > add_in_tensor(ir_node->input_num);
-        for (int i = 0; i < ir_node->input_num; i++)
-        {
-            struct tensor* input_tensor = get_ir_graph_tensor(ir_graph, ir_node->input_tensors[i]);
-            add_in_tensor[i] = this->vx_tensor_map[input_tensor->index];
-        }
-        struct tensor* output_tensor = get_ir_graph_tensor(ir_graph, ir_node->output_tensors[0]);
+        struct tensor* input_tensor = get_ir_graph_tensor(ir_graph, ir_node->input_tensors[i]);
+        add_in_tensor[i] = this->vx_tensor_map[input_tensor->index];
+    }
+    struct tensor* output_tensor = get_ir_graph_tensor(ir_graph, ir_node->output_tensors[0]);
 
-        eltwise_param* param = (eltwise_param*)ir_node->op.param_mem;
-
-        switch (param->type)
-        {
-            case ELT_PROD:
-            {
-                auto eltmul = graph->CreateOperation<tim::vx::ops::Multiply>(1);
-                (*eltmul)
-                        .BindInputs(add_in_tensor)
-                        .BindOutputs({ this->vx_tensor_map[output_tensor->index] });
-                break;
-            }
-            case ELT_SUM:
-            {
-                auto eltsum = graph->CreateOperation<tim::vx::ops::Add>();
-                (*eltsum)
-                        .BindInputs(add_in_tensor)
-                        .BindOutputs({ this->vx_tensor_map[output_tensor->index] });
-                break;
-            }
-            case ELT_SUB:
-            {
-                auto eltsub = graph->CreateOperation<tim::vx::ops::Sub>();
-                (*eltsub)
-                        .BindInputs(add_in_tensor)
-                        .BindOutputs({ this->vx_tensor_map[output_tensor->index] });
-                break;
-            }
-            case ELT_EXP:
-            {
-                auto eltexp = graph->CreateOperation<tim::vx::ops::Exp>();
-                (*eltexp)
-                        .BindInputs(add_in_tensor)
-                        .BindOutputs({ this->vx_tensor_map[output_tensor->index] });
-                break;
-            }
-            default:
-                break;
-        }
+    switch (param->type)
+    {
+    case ELT_PROD:
+    case ELT_PROD_SCALAR:
+    {
+        auto eltmul = graph->CreateOperation<tim::vx::ops::Multiply>(1);
+        (*eltmul)
+            .BindInputs(add_in_tensor)
+            .BindOutputs({ this->vx_tensor_map[output_tensor->index] });
+        break;
+    }
+    case ELT_SUM:
+    case ELT_SUM_SCALAR:
+    {
+        auto eltsum = graph->CreateOperation<tim::vx::ops::Add>();
+        (*eltsum)
+            .BindInputs(add_in_tensor)
+            .BindOutputs({ this->vx_tensor_map[output_tensor->index] });
+        break;
+    }
+    case ELT_SUB:
+    case ELT_SUB_SCALAR:
+    {
+        auto eltsub = graph->CreateOperation<tim::vx::ops::Sub>();
+        (*eltsub)
+            .BindInputs(add_in_tensor)
+            .BindOutputs({ this->vx_tensor_map[output_tensor->index] });
+        break;
+    }
+    case ELT_EXP:
+    {
+        auto eltexp = graph->CreateOperation<tim::vx::ops::Exp>();
+        (*eltexp)
+            .BindInputs(add_in_tensor)
+            .BindOutputs({ this->vx_tensor_map[output_tensor->index] });
+        break;
+    }
+    default:
+        break;
     }
 
 
 
     return 0;
 }
-
