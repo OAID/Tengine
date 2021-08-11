6eba612c388cd477cc4f4e7f92d18a768494ea72
FeiGeChuanShu <80833738+FeiGeChuanShu@users.noreply.github.com>
2021-08-11 16:52:51 +0800

    add gelu operator (#983)
    * Update op_name.h
    
    * Update op.h
    
    * Create gelu.c
    
    * apply code-format changes
    
    * Update tm2_format.h
    
    * Create tm2_gelu.c
    
    * apply code-format changes
    
    * Create gelu_ref.c
    
    * apply code-format changes
    
    * Update gelu_ref.c
    
    * Update tm2_format.h
    
    * Update onnx2tengine.cpp
    
    Co-authored-by: FeiGeChuanShu <FeiGeChuanShu@users.noreply.github.com>
    Co-authored-by: BUG1989 <248857878@qq.com>

---
+++ source/operator/prototype/gelu.c
@@ -0,0 +1,69 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * License); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * AS IS BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/*
+ * Copyright (c) 2021, OPEN AI LAB
+ * Author: 774074168@qq.com
+ */
+
+#include "api/c_api.h"
+#include "graph/tensor.h"
+#include "graph/node.h"
+#include "graph/graph.h"
+#include "module/module.h"
+#include "utility/sys_port.h"
+
+static int infer_shape(ir_node_t* node)
+{
+    ir_graph_t* ir_graph = node->graph;
+    ir_tensor_t* input = get_ir_graph_tensor(ir_graph, node->input_tensors[0]);
+    ir_tensor_t* output = get_ir_graph_tensor(ir_graph, node->output_tensors[0]);
+
+    set_ir_tensor_shape(output, input->dims, input->dim_num);
+
+    return 0;
+}
+
+static int init_op(ir_op_t* op)
+{
+    op->same_shape = 0;
+    op->infer_shape = infer_shape;
+
+    return 0;
+}
+
+static void release_op(ir_op_t* op)
+{
+}
+
+int register_gelu_op()
+{
+    ir_method_t m;
+
+    m.version = 1;
+    m.init = init_op;
+    m.release = release_op;
+
+    return register_op(OP_GELU, OP_GELU_NAME, &m);
+}
+
+int unregister_gelu_op()
+{
+    return unregister_op(OP_GELU, 1);
+}
